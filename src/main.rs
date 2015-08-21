f09e4869eb2d5bbc7f47b7f5a5624d68dc66dcef
Carol Nichols <carol.nichols@gmail.com>
2015-08-20 21:06:05 -0400

    3 circles of different colors

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,17 @@ fn main() {
         e.draw_2d(|c, g| {
            clear(target_color, g);
 
-           let original = Ellipse::new([1.0, 1.0, 1.0, 1.0]);
-           original.draw([50.0, 50.0, 500.0, 500.0], &c.draw_state, c.transform, g);
+           let playing_surface = Ellipse::new([1.0, 1.0, 1.0, 1.0]);
+           playing_surface.draw([50.0, 50.0, 500.0, 500.0], &c.draw_state, c.transform, g);
 
-           let original = Ellipse::new([1.0, 0.0, 0.0, 0.5]);
-           original.draw([300.0, 300.0, 100.0, 100.0], &c.draw_state, c.transform, g);
+           let candidate1 = Ellipse::new(target_color);
+           candidate1.draw([300.0, 300.0, 100.0, 100.0], &c.draw_state, c.transform, g);
+
+           let candidate2 = Ellipse::new([0.0, 1.0, 0.0, 1.0]);
+           candidate2.draw([100.0, 200.0, 100.0, 100.0], &c.draw_state, c.transform, g);
+
+           let candidate3 = Ellipse::new([0.0, 0.0, 1.0, 1.0]);
+           candidate3.draw([300.0, 100.0, 100.0, 100.0], &c.draw_state, c.transform, g);
         });
     }
 }
