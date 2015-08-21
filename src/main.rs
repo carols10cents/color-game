3cfacb5f87924090890dc105572751f057b04981
Carol Nichols <carol.nichols@gmail.com>
2015-08-20 21:02:03 -0400

    Draw the background with the target color

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,17 @@ fn main() {
         .build()
         .unwrap();
 
+    let target_color = [1.0, 0.0, 0.0, 1.0];
+
     for e in window {
         e.draw_2d(|c, g| {
-           clear([1.0; 4], g);
+           clear(target_color, g);
+
+           let original = Ellipse::new([1.0, 1.0, 1.0, 1.0]);
+           original.draw([50.0, 50.0, 500.0, 500.0], &c.draw_state, c.transform, g);
+
            let original = Ellipse::new([1.0, 0.0, 0.0, 0.5]);
-           original.draw([0.0, 0.0, 100.0, 100.0], &c.draw_state, c.transform, g);
+           original.draw([300.0, 300.0, 100.0, 100.0], &c.draw_state, c.transform, g);
         });
     }
 }
