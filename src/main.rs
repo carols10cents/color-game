4f128a2f44b9aaf0d7309595e58edc3739e65c76
Carol Nichols <carol.nichols@gmail.com>
2015-08-20 20:55:50 -0400

    We drew an ellipse as wide as it is high!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate piston_window;
+
+use piston_window::*;
+
 fn main() {
-    println!("Hello, world!");
+    let window: PistonWindow = WindowSettings::new(
+            "colors",
+            [600, 600]
+        )
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    for e in window {
+        e.draw_2d(|c, g| {
+           clear([1.0; 4], g);
+           let original = Ellipse::new([1.0, 0.0, 0.0, 0.5]);
+           original.draw([0.0, 0.0, 100.0, 100.0], &c.draw_state, c.transform, g);
+        });
+    }
 }
